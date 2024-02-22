ebbcf519a920364aef475a466514f556b9fedbb9
Julio J. Valera Rodriguez <jvalera@perforosven.com>
2024-02-22 01:17:35 -0400

    six commit

--- src/pages/PokeIdPage.jsx
+++ src/pages/PokeIdPage.jsx
@@ -3,6 +3,7 @@ import useFetch from '../store/hooks/useFetch';
 import { useParams } from 'react-router-dom';
 import ProgressBar from "@ramonak/react-progress-bar";
 import './styles/pokeIdPage.css';
+import { HeaderImg } from './HeaderImg';
 
 const PokeIdPage = () => {
     const [pokeData, getPokeData] = useFetch();
@@ -13,95 +14,113 @@ const PokeIdPage = () => {
         getPokeData(url);
     }, []);
 
+    const pokemonName = pokeData?.name;
     const abilities = pokeData?.abilities;
     const types = pokeData?.types;
     const moves = pokeData?.moves;
+    const height = pokeData?.height;
+    const weight = pokeData?.weight;
+    const pokemonImg = pokeData?.sprites.other['official-artwork'].front_default;
 
     const stats = pokeData?.stats.filter(i => {
         return !i.stat.name.includes('special');
     });
     
-    // console.log(pokeData?.moves)
-    console.log(moves)
     return (
-        <article>
-            <section>
-                <figure className='img'>
-                    <img
-                        src={pokeData?.sprites.other['official-artwork'].front_default}
-                        alt={pokeData?.name}
-                    />
+        <article className="container">
+            <HeaderImg className='header__img'/>
+
+            <div className="card">
+                <div className="gradient"></div>
+
+                <figure>
+                    <img src={pokemonImg} alt={pokemonName} />
                 </figure>
 
-                <div className='id'>
-                    <hr />
-                    <h1>#{param.id}</h1>
-                    <hr />
+                <hr />
+                <h1>#{param.id}</h1>
+                <hr />
+
+                <h2 className="title">{pokemonName}</h2>
+
+                <div className="corporal__units">
+                    <div className="individual__unit">
+                        <span className='peso'><strong>Peso</strong></span>
+                        <span className='individual'>{weight}</span>
+                    </div>
+
+                    <div className="individual__unit">
+                        <span className='altura'><strong>Altura</strong></span>
+                        <span className='individual'>{height}</span>
+                    </div>
                 </div>
 
-                <div className='name'>
-                    <h3>{pokeData?.name}</h3>
-                </div>
-                <br />
-                <section>
-                    <div className='tipo'>
+                <section className="poke__type__container">
+                    <div>
                         <h3>Tipo</h3>
                         <br />
-                        <ul>
-                            {types?.map(type => (
+                        <ul className='poke__type__card'>
+                            {types?.map((type) => (
                                 <li key={type.type.url}>
-                                    <span>{type.type.name}</span>
+                                    <span className='poke__type'>
+                                        {type.type.name}
+                                    </span>
                                 </li>
                             ))}
                         </ul>
                     </div>
-                    <div className='skills'>
+
+                    <div>
                         <h3>Habilidades</h3>
                         <br />
-                        <ul>
+                        <ul className='poke__type__card'>
                             {abilities?.map(ability => (
                                 <li key={ability.ability.url}>
-                                    <span>{ability.ability.name}</span>
+                                    <span className='poke__type'>{ability.ability.name}</span>
                                 </li>
                             ))}
                         </ul>
                     </div>
                 </section>
-                <br />
-                <section className='statistics'>
+
+                <section className="stats__section">
                     <div>
                         <h2>Stats</h2>
                     </div>
-                    <br />
+
                     <ul>
                         {stats?.map(stat => (
                             <li key={stat.stat.url}>
-                                <span>{stat.stat.name}:</span>
-                                <span>{stat.base_stat}/100</span>
+                                <div className="stat__labels">
+                                    <span className='poke__type'>{stat.stat.name}:</span>
+                                    <span className='poke__type stat-numbre'>{stat.base_stat}/100</span>
+                                </div>
+
                                 <ProgressBar
                                     completed={`${stat.base_stat}`}
+                                    bgColor='black'
                                 />
                             </li>
                         ))}
                     </ul>
                 </section>
-            </section>
-            <br />
-            <section className='movements'>
+            </div>
+
+            <section className="card">
                 <div>
                     <h2>Movements</h2>
                     <hr />
                 </div>
-                <br />
-                <ul className='list'>
+
+                <ul className='grid-list'>
                     {moves?.map(m => (
                         <li key={m.move.url}>
-                            <span>{m.move.name}</span>
+                            <span className='tag black-text'>{m.move.name}</span>
                         </li>
                     ))}
                 </ul>
             </section>
-        </article>
+        </article >
     );
 };
 
